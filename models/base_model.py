73f91fc274d68c2a6df7fc085db1fbfd301e2833
sketchturnerr <aFhs7ajk@yandex.ru>
2016-04-10 12:50:50 +0700

    Optimize import.

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,9 @@
 from conf import Conf
-from playhouse.postgres_ext import Field, Model
-from playhouse.db_url import connect, PooledPostgresqlExtDatabase
+from playhouse.postgres_ext import Model
+from playhouse.db_url import connect
 
 db = connect(Conf.get('db_connection_string'), register_hstore=False)
+
 
 class BaseModel(Model):
     class Meta:
