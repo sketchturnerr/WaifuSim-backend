6f368137e7e5cd22df1a73ff5a5f07ca42da444d
sketchturnerr <aFhs7ajk@yandex.ru>
2016-04-10 20:10:22 +0700

    Add jsonifier method.

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+from  datetime import datetime
 from conf import Conf
 from playhouse.postgres_ext import Model
 from playhouse.db_url import connect
@@ -8,3 +9,13 @@ db = connect(Conf.get('db_connection_string'), register_hstore=False)
 class BaseModel(Model):
     class Meta:
         database = db
+
+    def to_json(self):
+        json = {}
+        for key in self._data.keys():
+            value = getattr(self, key)
+            if isinstance(value, datetime):
+                value = value.timestamp()
+            if isinstance(value, BaseModel): continue
+            json[key] = value
+        return json
