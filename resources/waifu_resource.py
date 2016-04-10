706935a11c3fc3e510ea6a87eac9608a159d4b8b
sketchturnerr <aFhs7ajk@yandex.ru>
2016-04-10 21:12:09 +0700

    Add ability to get waifu.

--- resources/waifu_resource.py
+++ resources/waifu_resource.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import falcon
 from models.base_model import db
-from models.waifu_model import WaifuModel
+from models.waifu_model import WaifuModel, WAIFU_SHARING_STATUS_PUBLIC
 from resources.middlewares.auth import auth
 from resources.middlewares.body_checker import body_checker
 
@@ -13,7 +13,7 @@ DOCS_LINK = 'https://github.com/sketchturnerr/WaifuSim-backend#waifu'
 class WaifuCollectionResource(object):
 
     @falcon.before(auth())
-    @falcon.before(body_checker(('name', 'description', 'pic', ), DOCS_LINK))
+    @falcon.before(body_checker(('name', 'description', 'pic',), DOCS_LINK))
     def on_post(self, req, resp):
         with db.atomic():
             waifu = WaifuModel(**req.context['parsed_body'])
@@ -25,17 +25,25 @@ class WaifuCollectionResource(object):
 class WaifuResource(object):
 
     @falcon.before(auth())
-    def on_get(self, req, resp):
-        pass
+    def on_get(self, req, resp, id):
+        try:
+            waifu = WaifuModel.get(
+                (WaifuModel.id == id) &
+                ((WaifuModel.owner == req.context['user']) |
+                 (WaifuModel.sharing_status == WAIFU_SHARING_STATUS_PUBLIC))
+            )
+        except ValueError:
+            # id не int, что ж, у нас явно нет такого документа.
+            raise falcon.HTTPNotFound()
+        resp.body = json.dumps(waifu.to_json())
 
     @falcon.before(auth())
-    @falcon.before(body_checker(('name', 'description', 'pic', ), DOCS_LINK))
+    @falcon.before(body_checker(('name', 'description', 'pic',), DOCS_LINK))
     def on_put(self, req, resp, id):
         with db.atomic():
             try:
                 waifu = WaifuModel.get(WaifuModel.id == id, WaifuModel.owner == req.context['user'])
             except ValueError:
-                # id не int, что ж, у нас явно нет такого документа.
                 raise falcon.HTTPNotFound()
             waifu.name = req.context['parsed_body']['name']
             waifu.description = req.context['parsed_body']['description']
@@ -43,4 +51,3 @@ class WaifuResource(object):
             waifu.updated_at = datetime.now()
             waifu.save()
         resp.body = json.dumps(waifu.to_json())
-
--- resources/waifu_resource_test.py
+++ resources/waifu_resource_test.py
@@ -4,6 +4,8 @@ from time import sleep
 from app import api
 from create_tables import create_tables
 from falcon.testing import TestCase
+from models.base_model import db
+from models.waifu_model import WaifuModel, WAIFU_SHARING_STATUS_PUBLIC
 
 
 def with_token_query_string(func):
@@ -51,7 +53,7 @@ class WaifuResourceTest(TestCase):
     @with_token_query_string
     def test_update_waifu_should_return_not_found_for_not_existing_waifu(self, token_qs):
         body = json.dumps({'name': 'foo', 'description': 'bar', 'pic': 'baz'})
-        resp = self.simulate_put('/waifu/99999999', query_string=token_qs, body=body)
+        resp = self.simulate_put('/waifu/-1', query_string=token_qs, body=body)
         self.assertEqual(resp.status_code, 404)
         resp = self.simulate_put('/waifu/fff', query_string=token_qs, body=body)
         self.assertEqual(resp.status_code, 404)
@@ -74,3 +76,34 @@ class WaifuResourceTest(TestCase):
         self.assertEqual(resp.json.get('description'), 'bar1')
         self.assertEqual(resp.json.get('pic'), 'baz1')
         self.assertGreater(resp.json.get('updated_at')-resp.json.get('created_at'), 1)
+
+    @with_token_query_string
+    def test_get_waifu_should_return_not_found(self, token_qs):
+        resp = self.simulate_get("/waifu/-1", query_string=token_qs)
+        self.assertEqual(resp.status_code, 404)
+
+    @with_token_query_string
+    def test_get_waifu_should_return_not_found_for_non_public_waifu(self, token_qs):
+        id, _ = self._create_waifu(token_qs)
+        resp = self.simulate_post('/user')
+        token = resp.json.get('token')
+        resp = self.simulate_get("/waifu/%s" % id, query_string="token=%s" % token)
+        self.assertEqual(resp.status_code, 404)
+
+    @with_token_query_string
+    def test_get_waifu_should_return_waifu_object_for_owner(self, token_qs):
+        id, _ = self._create_waifu(token_qs)
+        resp = self.simulate_get("/waifu/%s" % id, query_string=token_qs)
+        self.assertEqual(resp.json.get('id'), id)
+
+    @with_token_query_string
+    def test_get_waifu_should_return_waifu_object_for_oublic_waifu(self, token_qs):
+        id, _ = self._create_waifu(token_qs)
+        with db.atomic():
+            waifu = WaifuModel.get(WaifuModel.id == id)
+            waifu.sharing_status = WAIFU_SHARING_STATUS_PUBLIC
+            waifu.save()
+        resp = self.simulate_post('/user')
+        token = resp.json.get('token')
+        resp = self.simulate_get("/waifu/%s" % id, query_string="token=%s" % token)
+        self.assertEqual(resp.json.get('id'), id)
