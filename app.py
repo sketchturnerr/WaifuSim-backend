948c9c6ffb8a34e3acf00b8190bf65504f2bfaf6
sketchturnerr <aFhs7ajk@yandex.ru>
2016-04-10 15:35:53 +0700

    Add endpoints for all resources.

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import falcon
+from resources.user_resource import UserResource, UserAuthResource
 from resources.waifu_message_resource import WaifuMessageResource
+from resources.waifu_resource import WaifuResource
 
 api = falcon.API()
+
+api.add_route('/user', UserResource())
+api.add_route('/user/auth', UserAuthResource())
+api.add_route('/waifu', WaifuResource())
 api.add_route('/waifu/messages', WaifuMessageResource())
